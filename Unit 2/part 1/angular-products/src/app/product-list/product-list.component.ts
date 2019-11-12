e2d5f1e545b619af2c4ed6ce475e0b3681ded5ae
Arturo <arturo@iessanvicente.com>
2019-11-12 14:50:20 +0100

    part 1 angular

---
+++ Unit 2/part 1/angular-products/src/app/product-list/product-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../interfaces/i-product';
+
+@Component({
+  selector: 'product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  title = 'My product\'s list';
+  headers = {
+    image: 'Image',
+    description: 'Product',
+    price: 'Price',
+    available: 'Available'
+  };
+  products: IProduct[] = [
+    {
+      id: 1,
+      description: 'SSD hard drive',
+      available: new Date('2016-10-03'),
+      price: 75,
+      imageUrl: 'assets/ssd.jpg',
+      rating: 5
+    },
+    {
+      id: 2,
+      description: 'LGA1151 Motherboard',
+      available: new Date('2016-09-15'),
+      price: 96.95,
+      imageUrl: 'assets/motherboard.jpg',
+      rating: 4
+    }
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
